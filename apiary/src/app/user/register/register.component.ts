eb3e81e24ee3e98ae1281dd37bfcb52c3e519658
Georgi <gogolino1982@gmail.com>
2024-03-28 21:11:11 +0200

    register
    working on Css and validators

--- apiary/src/app/user/register/register.component.ts
+++ apiary/src/app/user/register/register.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: "app-register",
+    templateUrl: "./register.component.html",
+    styleUrls: ["./register.component.css"],
 })
 export class RegisterComponent {
+    mandatoryFields: boolean = true;
 
+    form = this.fb.group({
+        email: ["", [Validators.required]],
+        username: ["", [Validators.required, Validators.minLength(4)]],
+        passGroup: this.fb.group(
+            {
+                password: ["", [Validators.required, Validators.minLength(4)]],
+                "repeat-password": [
+                    "",
+                    [Validators.required, Validators.minLength(4)],
+                ],
+            },
+            // TODO Validators
+        ),
+    });
+    constructor(private fb: FormBuilder) {}
+
+    register(): void {
+        if (this.form.invalid) {
+            return;
+        }
+        console.log(this.form.value);
+    }
 }
