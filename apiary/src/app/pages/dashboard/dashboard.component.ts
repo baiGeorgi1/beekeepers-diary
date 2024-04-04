1ba1df83bdc31ae36305d975254432e7a60c3c69
Georgi Georgiev <gogolino1982@gmail.com>
2024-04-04 15:11:30 +0300

    vertical-Nav -finished

--- apiary/src/app/pages/dashboard/dashboard.component.ts
+++ apiary/src/app/pages/dashboard/dashboard.component.ts
@@ -1,48 +1,53 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { GlobalLoaderService } from "src/app/core/global-loader/global-loader.service";
-import { ItemService } from "src/app/services/item.service";
-import { UserService } from "src/app/services/user.service";
-import { Hives } from "src/app/types/hives";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GlobalLoaderService } from 'src/app/core/global-loader/global-loader.service';
+import { ItemService } from 'src/app/services/item.service';
+import { UserService } from 'src/app/services/user.service';
+import { Hives } from 'src/app/types/hives';
 
 @Component({
-    selector: "app-dashboard",
-    templateUrl: "./dashboard.component.html",
-    styleUrls: ["./dashboard.component.css"],
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-    hives: Hives[] = [];
+  hives: Hives[] = [];
 
-    get userId(): string {
-        return this.userService.getUser()._id || "";
+  get userId(): string {
+    return this.userService.getUser()._id || '';
+  }
+
+  constructor(
+    private globalLoader: GlobalLoaderService,
+    private userService: UserService,
+    private api: ItemService
+  ) {}
+  ngOnInit(): void {
+    // this.globalLoader.showLoader();
+    // setTimeout(() => {
+    //     this.globalLoader.hideLoader();
+    // }, 2500);
+
+    const result = this.api.getItems(this.userId).subscribe({
+      next: (items: Hives[]) => {
+        console.log('ITEMS', items, this.userId);
+      },
+      complete: () => {
+        // console.log("Completed: ", this.hives);
+      },
+    });
+  }
+
+  clicked(event: any) {
+    const target = event.target.parentElement;
+    const vertNavBtns = document.querySelectorAll('.vert-nav');
+    for (let i = 0; i < vertNavBtns.length; i++) {
+      const item = vertNavBtns[i].parentElement;
+      console.log(item);
+      item!.classList.remove('active');
     }
-
-    constructor(
-        private globalLoader: GlobalLoaderService,
-        private userService: UserService,
-        private api: ItemService,
-    ) {}
-    ngOnInit(): void {
-        // this.globalLoader.showLoader();
-        // setTimeout(() => {
-        //     this.globalLoader.hideLoader();
-        // }, 2500);
-
-        const result = this.api.getItems(this.userId).subscribe({
-            next: (items: Hives[]) => {
-                console.log("ITEMS", items, this.userId);
-            },
-            complete: () => {
-                // console.log("Completed: ", this.hives);
-            },
-        });
-        // .subscribe((items) => {
-        //     console.log("ITEMS:", items);
-        //     items.forEach((item) => {
-        //         console.log(item.frames);
-
-        //         // find((s)=>{s===this.userId})
-        //     });
-        // });
-    }
+    target.classList.add('active');
+    console.log(target);
+    console.log(vertNavBtns);
+  }
 }
--- apiary/src/app/services/item.service.ts
+++ apiary/src/app/services/item.service.ts
@@ -1,25 +1,26 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { environment } from "src/environments/environment.development";
-import { Hives } from "../types/hives";
-import { Observable, Subscription, map } from "rxjs";
-import { UserService } from "./user.service";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { Hives } from '../types/hives';
+import { Observable, Subscription, map } from 'rxjs';
+import { UserService } from './user.service';
+import { User } from '../types/user';
 
 const { itemURL } = environment;
 @Injectable({
-    providedIn: "root",
+  providedIn: 'root',
 })
 export class ItemService {
-    constructor(private http: HttpClient, private userService: UserService) {}
-    result$!: Subscription;
-    getItems(userId: string): Observable<Hives[]> {
-        //TODO
-        console.log("HERE", userId);
-
-        this.result$ = this.http.get<Hives[]>(`${itemURL}/hives`).subscribe({
-            next: (item) => {
-                console.log(item);
-            },
-        });
-    }
+  constructor(private http: HttpClient, private userService: UserService) {}
+  result$!: Subscription;
+  getItems(userId: string): Observable<Hives[]> {
+    //TODO
+    console.log('HERE', userId);
+    return this.http.get<Hives[]>(`${environment.itemURL}/hives`);
+    // this.result$ = this.http.get<Hives[]>(`${itemURL}/hives`).subscribe({
+    //     next: (item) => {
+    //         console.log(item);
+    //     },
+    // });
+  }
 }
